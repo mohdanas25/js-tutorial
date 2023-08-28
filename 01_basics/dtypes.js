ca8a7fb538d81777c5f787a215d660bc9d8ffef1
Mohd Anas <112649085+mohdanas25@users.noreply.github.com>
2023-08-28 18:01:48 +0530

    Container Config

---
+++ 01_basics/dtypes.js
@@ -0,0 +1,20 @@
+"use strict";   //treat all code as newer version of js
+
+// alert(3+3); //we are using node js not browser
+
+let name = "Anas"
+let age = 21
+let isLoggedin = true
+
+//number 
+//bigint
+//string
+//boolean
+//null
+//undefined
+//symbol
+
+
+//non primitive
+
+//object
